369edda1567dbd5bc649d40bb549b677e26063ee
Edgar Sidawi <romantibuai@gmail.com>
2019-10-30 17:51:24 +0000

    fixed footer position

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,10 +1,23 @@
 <template>
-  <footer></footer>
+  <footer class="bg-primary">
+    <div class="d-flex justify-content-between">
+      <h8 class="mx-3">Realtime App</h8>
+      <h9 class="small">©2019</h9>
+    </div>
+  </footer>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped>
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  /* background-color: #333; */
+  color: #fff;
+  height: 4%;
+}
 </style>
