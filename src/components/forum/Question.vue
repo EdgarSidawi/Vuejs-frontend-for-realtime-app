142807728d68f34498c1d2dcc5900af29f932566
Edgar Sidawi <romantibuai@gmail.com>
2019-11-04 20:00:39 +0000

    added link to question component

--- src/components/forum/Question.vue
+++ src/components/forum/Question.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="card card-default">
     <div class="card-body">
-      <h3 class="card-title">{{question.title}}</h3>
+      <h3 class="card-title">
+        <router-link :to="question.path">{{question.title}}</router-link>
+      </h3>
       <div class="card-subtitle" style="color: grey">{{question.created_at}}</div>
       <div class="card-text my-3">{{question.body}}</div>
     </div>
