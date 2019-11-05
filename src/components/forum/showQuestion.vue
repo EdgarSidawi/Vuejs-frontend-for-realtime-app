8f2d909305b1f951521b7d6d57dbc77f7f3f69d5
Edgar Sidawi <romantibuai@gmail.com>
2019-11-05 22:54:04 +0000

    button created

--- src/components/forum/showQuestion.vue
+++ src/components/forum/showQuestion.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="card card-default">
     <div class="card-body">
-      <div class="card-title">
-        <h3>{{question.title}}</h3>
-        <div class="card-subtitle" style="color:grey">{{question.user}} said {{question.created_at}}</div>
+      <div class="card-title d-flex justify-content-between">
+        <div>
+          <h3>{{question.title}}</h3>
+          <div
+            class="card-subtitle"
+            style="color:grey"
+          >{{question.user}} said {{question.created_at}}</div>
+        </div>
+        <div class="btn btn-primary my-auto mx-5" style="text-align: center, color: teal">5 replies</div>
       </div>
       <div class="card-text py-4">{{question.body}}</div>
     </div>
