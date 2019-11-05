802aefee7f8c99933a1fb7d2daa52833811c917b
Edgar Sidawi <romantibuai@gmail.com>
2019-11-05 22:37:16 +0000

    show question page

--- src/components/forum/showQuestion.vue
+++ src/components/forum/showQuestion.vue
@@ -4,7 +4,7 @@
       <div class="card-title">
         <h3>{{question.title}}</h3>
       </div>
-      <div class="card-subtitle" style="color: grey">{{question.user}} said {{question.created_at}}</div>
+      <div class="card-subtitle" style="color:grey">{{question.user}} said {{question.created_at}}</div>
       <div class="card-text py-5">{{question.body}}</div>
     </div>
   </div>
