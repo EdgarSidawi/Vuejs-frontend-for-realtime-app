e162b40afac46af296a49cfd84675a44235a29c4
Edgar Sidawi <romantibuai@gmail.com>
2019-11-06 19:49:39 +0000

    integrated vue simple mde

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import axios from 'axios';
 import { store } from './store/store';
+import VueSimplemde from 'vue-simplemde';
+import 'simplemde/dist/simplemde.min.css';
 
 import router from './Router/router';
 
 Vue.config.productionTip = false;
+
+Vue.component('vue-simplemde', VueSimplemde);
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
