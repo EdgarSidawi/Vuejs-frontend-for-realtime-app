61ca9be5e338eddcd1621883cf781807ee2fee12
Edgar Sidawi <romantibuai@gmail.com>
2019-11-10 16:00:31 +0000

    destroy method for create category

--- src/components/category/CreateCategory.vue
+++ src/components/category/CreateCategory.vue
@@ -6,14 +6,26 @@
           <label for="category"></label>
           <input
             type="text"
-            v-model="form.category"
-            name="category"
+            v-model="form.name"
+            name="name"
             class="form-control"
             placeholder="Category Name"
           />
         </div>
         <button type="submit" class="btn btn-primary">Create</button>
       </form>
+    </div>
+
+    <header class="text-center text-light border bg-primary py-2 my-5">CATEGORIES</header>
+    <div class="list-group list-group-flush">
+      <div class="list-group-item" v-for="(category,index) in categories" :key="category.id">
+        {{category.name}}
+        <div
+          class="button btn btn-danger float-right mx-2"
+          @click="destroy(category.slug,index)"
+        >Delete</div>
+        <div class="button btn btn-warning float-right mx-2">Edit</div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,13 +37,27 @@ export default {
   data() {
     return {
       form: {
-        category: null
-      }
+        name: null
+      },
+      categories: {}
     };
+  },
+  created() {
+    Axios.get("api/category").then(res => {
+      this.categories = res.data.data;
+    });
   },
   methods: {
     create() {
-      Axios.post("api/category", this.form).then(res => console.log(res.data));
+      Axios.post("/api/category", this.form).then(res => {
+        this.categories.unshift(res.data);
+        this.form.name = null;
+      });
+    },
+    destroy(slug, index) {
+      Axios.delete(`api/category/${slug}`).then(
+        this.categories.splice(index, 1)
+      );
     }
   }
 };
