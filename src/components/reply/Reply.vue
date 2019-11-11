cf42e5cb5c6905a36779e166b36372fef35252f5
Edgar Sidawi <romantibuai@gmail.com>
2019-11-11 15:13:58 +0000

    buttons for edit and delete in reply component

---
+++ src/components/reply/Reply.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card mt-2">
+    <div class="card-body">
+      <div class="card-title">
+        <strong>{{reply.user}}</strong>
+        said {{reply.created_at}}
+      </div>
+      <div class="card-text" v-html="reply.reply"></div>
+      <div>
+        <hr />
+        <button class="btn btn-warning mr-2">
+          <small>edit</small>
+        </button>
+        <button class="btn btn-danger">
+          <small>Delete</small>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["reply"]
+};
+</script>
+
+<style>
+</style>
