eeec70d4a9cf32a8d5e334ffa7b5de0dfb0b2cef
Edgar Sidawi <romantibuai@gmail.com>
2019-11-12 13:33:25 +0000

    update replies component

--- src/components/reply/Replies.vue
+++ src/components/reply/Replies.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <reply v-for="reply in content" :key="reply.id" :reply="reply"></reply>
+    <reply v-for="reply in replies" :key="reply.id" :reply="reply"></reply>
   </div>
 </template>
 
@@ -9,15 +9,7 @@ import reply from "./Reply";
 
 export default {
   props: ["replies"],
-  data() {
-    return {
-      content: this.replies
-    };
-  },
-  components: { reply },
-  created() {
-    console.log("content: ", this.content);
-  }
+  components: { reply }
 };
 </script>
 
