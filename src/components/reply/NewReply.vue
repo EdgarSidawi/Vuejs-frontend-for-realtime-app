71752eb5fd09fcaeb9afc473c2b9794729ed3c76
Edgar Sidawi <romantibuai@gmail.com>
2019-11-12 11:41:48 +0000

    button added to new reply component

--- src/components/reply/NewReply.vue
+++ src/components/reply/NewReply.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container mt-4 mb-4">
     <vue-simplemde v-model="body" ref="markdownEditor" />
+    <div class="button btn btn-success mb-4">Reply</div>
   </div>
 </template>
 
