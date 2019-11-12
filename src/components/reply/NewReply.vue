a2620f386643e08d408a9598c93286ae63ee607c
Edgar Sidawi <romantibuai@gmail.com>
2019-11-12 11:57:51 +0000

    submit method for reply button

--- src/components/reply/NewReply.vue
+++ src/components/reply/NewReply.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="container mt-4 mb-4">
     <vue-simplemde v-model="body" ref="markdownEditor" />
-    <div class="button btn btn-success mb-4">Reply</div>
+    <div class="button btn btn-success mb-4" @click="submit">Reply</div>
   </div>
 </template>
 
 <script>
+import Axios from "axios";
+
 export default {
   props: ["questionSlug"],
   data() {
     return {
       body: null
     };
+  },
+  methods: {
+    submit() {
+      Axios.post(`api/question/${this.questionSlug}/reply`, this.body).then(
+        res => console.log(res)
+      );
+    }
   }
 };
 </script>
